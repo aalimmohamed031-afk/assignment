63a6266a0a2ed4974092dbff99b95afa13589e89
Ali Farouk <aalimmohamed031@gmail.com>
2026-07-11 21:35:36 +0300

    .
    Implement character counting in an array.

--- ASS_5/ass5.c
+++ ASS_5/ass5.c
@@ -1,11 +1,32 @@
-/*
- * TODO: Paste your actual code 5 here.
- * This is a placeholder until you send the real code.
- */
-
 #include <stdio.h>
 
-int main() {
-    printf("Assignment 5 - code not provided yet.\n");
-    return 0;
+int charactercount(char arr[], int size, char target){
+    int count =0;
+    for (int i=0;i<size;i++){
+        if(arr[i]==target){
+            count++;
+        }
+    }
+}
+int main(){
+    int size;
+    char target;
+    printf("enter the size of array");
+    scanf("%d",&size);
+    char arr[size];
+    
+    printf("enter the %d characters", size);
+    for(int i=0; i<size; i++){
+        printf("enter the %d character", i+1);
+        scanf("%c", &arr[i]);
+    }
+
+    printf("enter the target character to find its repeating count");
+    scanf("%c", &target);
+
+    int counting = charactercount(arr, size, target);
+
+    printf("the repeating count of target character is %d",counting);
+    
+return 0;
 }
